86709987074e76470005af0c7ec97cafdc8ebf73
= <=>
2025-09-25 18:18:27 -0400

    Update docstring in PauseModel to clarify bitmap usage in PauseView

--- Qubitrix/model/pause_model.py
+++ Qubitrix/model/pause_model.py
@@ -1,6 +1,7 @@
 class PauseModel:
     """
-    Stores the bitmap of the paused screen for display in PauseView.
+    Stores the bitmap of the paused screen for display in the
+    background of the PauseView.
     """
     def __init__(self, paused_bitmap=None):
         self.paused_bitmap = paused_bitmap
